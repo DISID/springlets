f351f26d12129602e3be3a9622312efe0fc40664
jmvivo <jmvivoa1@gmail.com>
2018-04-11 10:05:28 +0200

    Fixes #64 and Fixes #57

--- springlets-data/springlets-data-commons/src/main/java/io/springlets/data/web/datatables/ConvertedDatatablesData.java
+++ springlets-data/springlets-data-commons/src/main/java/io/springlets/data/web/datatables/ConvertedDatatablesData.java
@@ -3,14 +3,6 @@
  */
 package io.springlets.data.web.datatables;
 
-import io.springlets.data.web.datatables.DatatablesColumns.Column;
-
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.TypeDescriptor;
-import org.springframework.data.domain.Page;
-
 import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,18 +10,26 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.data.domain.Page;
+
+import io.springlets.data.web.datatables.DatatablesColumns.Column;
+
 /**
- * Response data for data requests performed by datatables component. 
- * 
+ * Response data for data requests performed by datatables component.
+ *
  * The data is converted through a {@link ConversionService} to a Map of String values with each
- * object bean property as the key. 
- * 
+ * object bean property as the key.
+ *
  * It may also take into account the columns parameters sent
  * by the datatables to convert only the needed properties.
- * 
+ *
  * The current {@link Locale} is also taken into account, so this class allows to send data to
  * a datatables using the same conversion which is used in the application pages.
- * 
+ *
  * @author Cèsar Ordiñana at http://www.disid.com[DISID Corporation S.L.]
  */
 public class ConvertedDatatablesData<T> extends DatatablesData<Map<String, Object>> {
@@ -37,17 +37,26 @@ public class ConvertedDatatablesData<T> extends DatatablesData<Map<String, Objec
   private static final TypeDescriptor TYPE_STRING = TypeDescriptor.valueOf(String.class);
 
   /**
+   * Default property separator for column definition.
+   * Property separator for column definition.
+   * THIS CAN'T BE "." (dot): Datatables can get problems if response contains a compund-with-dots fields
+   * see https://datatables.net/manual/tech-notes/4
+   */
+  public static final String DEFAULT_PROPERTY_SEPARATOR = "/";
+
+  /**
    * Create a response for datatables with data obtained from a previous request.
    *
    * @param data the data to show
    * @param recordsTotal the total number of available data
-   * @param draw counts datatables requests. It must be sent by datatables value 
+   * @param draw counts datatables requests. It must be sent by datatables value
    * in the data request.
    * @param conversionService to convert the data values to String
    */
   public ConvertedDatatablesData(Page<T> data, Long recordsTotal, Integer draw,
       ConversionService conversionService) {
-    this(data.getContent(), recordsTotal, data.getTotalElements(), draw, conversionService, null);
+    this(data.getContent(), recordsTotal, data.getTotalElements(), draw, conversionService, null,
+        DEFAULT_PROPERTY_SEPARATOR);
   }
 
   /**
@@ -55,15 +64,15 @@ public class ConvertedDatatablesData<T> extends DatatablesData<Map<String, Objec
    *
    * @param data the data to show
    * @param recordsTotal the total number of available data
-   * @param draw counts datatables requests. It must be sent by datatables value 
+   * @param draw counts datatables requests. It must be sent by datatables value
    * in the data request.
    * @param conversionService to convert the data values to String
    * @param columns parameters sent by datatables
    */
   public ConvertedDatatablesData(Page<T> data, Long recordsTotal, Integer draw,
       ConversionService conversionService, DatatablesColumns columns) {
-    this(data.getContent(), recordsTotal, data.getTotalElements(), draw, conversionService,
-        columns);
+    this(data.getContent(), recordsTotal, data.getTotalElements(), draw, conversionService, columns,
+        DEFAULT_PROPERTY_SEPARATOR);
   }
 
   /**
@@ -72,13 +81,14 @@ public class ConvertedDatatablesData<T> extends DatatablesData<Map<String, Objec
    * @param data the data to show
    * @param recordsTotal the total number of available data
    * @param recordsFiltered the number of data after filtering
-   * @param draw counts datatables requests. It must be sent by datatables value 
+   * @param draw counts datatables requests. It must be sent by datatables value
    * in the data request.
    * @param conversionService to convert the data values to String
    */
   public ConvertedDatatablesData(List<T> data, Long recordsTotal, Long recordsFiltered,
       Integer draw, ConversionService conversionService) {
-    this(data, recordsTotal, recordsFiltered, draw, conversionService, null);
+    this(data, recordsTotal, recordsFiltered, draw, conversionService, null,
+        DEFAULT_PROPERTY_SEPARATOR);
   }
 
   /**
@@ -87,18 +97,25 @@ public class ConvertedDatatablesData<T> extends DatatablesData<Map<String, Objec
    * @param data the data to show
    * @param recordsTotal the total number of available data
    * @param recordsFiltered the number of data after filtering
-   * @param draw counts datatables requests. It must be sent by datatables value 
+   * @param draw counts datatables requests. It must be sent by datatables value
    * in the data request.
    * @param conversionService to convert the data values to String
    * @param columns parameters sent by datatables
+   * @param propertySeparator String to use as separator when use properties of
+   *             object-properties (WARNING: don't use "." [dot]
+   *             see https://github.com/DISID/springlets/issues/64)
    */
   public ConvertedDatatablesData(List<T> data, Long recordsTotal, Long recordsFiltered,
-      Integer draw, ConversionService conversionService, DatatablesColumns columns) {
-    super(convertAll(data, conversionService, columns), recordsTotal, recordsFiltered, draw, null);
+      Integer draw, ConversionService conversionService, DatatablesColumns columns,
+      String propertySeparator) {
+    super(
+        convertAll(data, conversionService, columns,
+            propertySeparator != null ? propertySeparator : DEFAULT_PROPERTY_SEPARATOR),
+        recordsTotal, recordsFiltered, draw, null);
   }
 
   private static List<Map<String, Object>> convertAll(List<?> data,
-      ConversionService conversionService, DatatablesColumns columns) {
+      ConversionService conversionService, DatatablesColumns columns, String propertySeparator) {
 
     if (data == null) {
       return null;
@@ -108,7 +125,7 @@ public class ConvertedDatatablesData<T> extends DatatablesData<Map<String, Objec
 
     for (Object value : data) {
       Map<String, Object> convertedValue = columns == null ? convert(value, conversionService)
-          : convert(value, conversionService, columns);
+          : convert(value, conversionService, columns, propertySeparator);
       converted.add(convertedValue);
     }
 
@@ -137,7 +154,7 @@ public class ConvertedDatatablesData<T> extends DatatablesData<Map<String, Objec
   }
 
   private static Map<String, Object> convert(Object value, ConversionService conversionService,
-      DatatablesColumns columns) {
+      DatatablesColumns columns, String propertySeparator) {
 
     BeanWrapper bean = new BeanWrapperImpl(value);
     Map<String, Object> convertedValue = new HashMap<>();
@@ -145,53 +162,31 @@ public class ConvertedDatatablesData<T> extends DatatablesData<Map<String, Objec
     for (Column column : columns.getColumns()) {
       String property = column.getData();
       convertedValue.put(property,
-          convertProperty(bean, property, conversionService, convertedValue));
+          convertProperty(bean, property, conversionService, propertySeparator));
     }
 
     return convertedValue;
   }
 
   private static Object convertProperty(BeanWrapper parentBean, String property,
-      ConversionService conversionService, Map<String, Object> parentValue) {
+      ConversionService conversionService, String propertySeparator) {
 
-    TypeDescriptor source = parentBean.getPropertyTypeDescriptor(property);
-    Object propertyValue = parentBean.getPropertyValue(property);
-
-    int dotIndex = property.indexOf('.');
+    int dotIndex = property.indexOf(propertySeparator);
     if (dotIndex > 0) {
       String baseProperty = property.substring(0, dotIndex);
-      String childProperty = property.substring(dotIndex + 1);
-      Map<String, Object> childValue = getChildValue(parentValue, baseProperty, childProperty);
+      String childProperty = property.substring(dotIndex + propertySeparator.length());
+
       BeanWrapper childBean = new BeanWrapperImpl(parentBean.getPropertyValue(baseProperty));
-      Object convertedProperty =
-          convertProperty(childBean, childProperty, conversionService, childValue);
-      childValue.put(childProperty, convertedProperty);
-      return childValue;
+      return convertProperty(childBean, childProperty, conversionService, propertySeparator);
     } else {
-      String convertedPropertyValue =
-          (String) conversionService.convert(propertyValue, source, TYPE_STRING);
-      return convertedPropertyValue;
-    }
-  }
+      TypeDescriptor source = parentBean.getPropertyTypeDescriptor(property);
+      Object propertyValue = parentBean.getPropertyValue(property);
 
-  @SuppressWarnings("unchecked")
-  private static Map<String, Object> getChildValue(Map<String, Object> parentValue,
-      String baseProperty, String childProperty) {
-    Map<String, Object> childValue = null;
-
-    if (parentValue.containsKey(childProperty)) {
-      Object value = parentValue.get(baseProperty);
-      if (value instanceof Map<?, ?>) {
-        childValue = (Map<String, Object>) value;
+      if (source.isAssignableTo(TYPE_STRING)) {
+        return (String) propertyValue;
+      } else {
+        return (String) conversionService.convert(propertyValue, source, TYPE_STRING);
       }
     }
-
-    if (childValue == null) {
-      childValue = new HashMap<>();
-      parentValue.put(baseProperty, childValue);
-    }
-
-    return childValue;
   }
-
 }
